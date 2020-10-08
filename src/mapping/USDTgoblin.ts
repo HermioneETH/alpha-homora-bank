a33a7930a41c67ee3d111d0d1b46043ea383ced2
hermione <hermioneeth@gmail.com>
2020-10-08 16:04:08 +0700

    Alpha homora subgraph

---
+++ src/mapping/USDTgoblin.ts
@@ -0,0 +1,25 @@
+import { BigInt, Address, ethereum } from "@graphprotocol/graph-ts"
+import {
+  UniswapGoblin,
+  Reinvest,
+  AddShare,
+  RemoveShare,
+  Liquidate
+} from "../../generated/USDTGoblin/UniswapGoblin"
+import { Reinvest as ReinvestHistory } from "../../generated/schema"
+
+export function handleReinvest(event: Reinvest): void {
+  let reinvest = new ReinvestHistory(event.transaction.hash.toHexString())
+  reinvest.caller = event.params.caller
+  reinvest.reward = event.params.reward
+  reinvest.bounty = event.params.bounty
+  reinvest.blockTime = event.block.timestamp;
+  reinvest.goblin = event.address;
+  reinvest.save()
+}
+
+export function handleAddShare(event: AddShare): void { }
+
+export function handleRemoveShare(event: RemoveShare): void { }
+
+export function handleLiquidate(event: Liquidate): void { }
